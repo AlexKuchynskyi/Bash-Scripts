d5828046cf1451f5c90a13787769ef65bae84c52
Alex_K <33528960+AlexKuchynskyi@users.noreply.github.com>
2019-03-27 18:20:49 +0200

    Update fild_link.sh

--- fild_link.sh
+++ fild_link.sh
@@ -7,21 +7,40 @@
 # make sure about correct input
 if [ -z $1 ]
 then
-        echo "Usage: $0   file_name ..."
+    echo "Usage: $0   file_name ..."
 else
-	echo "Input is correct..."
-	# create multiple hardlinks...
-	for (( i=0; i<10; i++ )) ; do
-    		ln $1 $1_hard$i
-	done
-	# -c modifies output of stat command, %i gives Inode
-	INODE=$(stat -c '%i' $1) 
-	echo "Inode= $INODE"
-	echo "current path= $PWD/$1"
-	find / -inum $INODE  
-	echo "================================="
-	# '! -path' means except path...
-	# so we find all the files with specified inode  except our input file
-	# and remove them
-	find / -inum $INODE ! -path "$PWD/$1" -exec rm -f {} \;
+    echo "Input is correct..."
+    # create multiple hardlinks...
+    for (( i=0; i<10; i++ )) ; do
+        ln $1 $1_hard$i
+    done
+    # -c modifies output of stat command, %i gives Inode
+    INODE=$(stat -c '%i' $1) 
+    echo "Successfully created hard links... Inode= $INODE"
+    echo "current path= $PWD/$1"
+    find / -inum $INODE  
+    echo "================================="
+    # '! -path' means except path...
+    # so we find all the files with the specified inode
+    # except our input file and remove them
+    find / -inum $INODE ! -path "$PWD/$1" -exec rm -f {} \;
+    echo "Successfully removed all the hardlinks..."
+    
+    # create multiple symlinks...
+    echo "================================="
+    for (( i=0; i<10; i++ )) ; do
+        ln -s $1 $1_symlink$i
+    done
+    
+    echo "Successfully created syminks..."
+    # find all the symlinks which point to the original file
+    # BTW if specify the root directory as the directory to start from (/)
+    # the system comes to eternal loop and gives the error   !!!!!!!!!!!!!!!!!!  
+    find -L /root -samefile $PWD/$1  
+    echo "================================="
+    # '! -path' means except path...
+    # so we find all the files with the specified inode
+    # except our input file and remove them
+    #find / -inum $INODE ! -path "$PWD/$1" -exec rm -f {} \;
+    #echo "Successfully removed all the hardlinks..."
 fi
