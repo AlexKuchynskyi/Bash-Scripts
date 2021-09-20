ad8ff70cfbbfe13326f67c37ee3a52d17c90a6e5
Alex_K <33528960+AlexKuchynskyi@users.noreply.github.com>
2021-09-20 12:04:09 -0700

    Update restore_s3_objects.sh

--- restore_s3_objects.sh
+++ restore_s3_objects.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-recoverfiles=$(aws s3api list-object-versions --bucket <bucket_name>  \
---prefix <path/to/folder/> --query "DeleteMarkers[?IsLatest]")
+######################################
+# Script restores deleted objects from AWS S3 bucket.
+# Works for buckets with enabled bucket versioning.
+# Needs jq to be installed.
+#####################################
+
+recoverfiles=$(aws s3api list-object-versions --bucket <bucket_name> --prefix <path/to/folder/> --query "DeleteMarkers[?IsLatest]")
 # you may also select the specific date: "DeleteMarkers[?IsLatest && starts_with(LastModified,'yyyy-mm-dd')]"
-for row in  $(echo $recoverfiles | jq -c '.[]'); do
+
+for row in  $(echo "${recoverfiles}" | jq -c '.[]'); do
     key=$(echo "${row}" | jq -r '.Key'  )
     versionId=$(echo "${row}" | jq -r '.VersionId'  )
     echo $key 
