838affa25f9dac3a0dcab343f9f8d60af7e36ce4
Alex_K <33528960+AlexKuchynskyi@users.noreply.github.com>
2021-09-20 11:59:18 -0700

    Create restore_s3_objects.sh

---
+++ restore_s3_objects.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+recoverfiles=$(aws s3api list-object-versions --bucket <bucket_name>  \
+--prefix <path/to/folder/> --query "DeleteMarkers[?IsLatest]")
+# you may also select the specific date: "DeleteMarkers[?IsLatest && starts_with(LastModified,'yyyy-mm-dd')]"
+for row in  $(echo $recoverfiles | jq -c '.[]'); do
+    key=$(echo "${row}" | jq -r '.Key'  )
+    versionId=$(echo "${row}" | jq -r '.VersionId'  )
+    echo $key 
+    aws s3api delete-object --bucket <bucket_name> --key $key --version-id $versionId
+    echo "Recovered"
+    echo "-----"
+done
